716bfda9323308b13e65720c773321aee36d0b8c
YutaOikawa <oikenfight@gmail.com>
2018-09-01 18:53:48 +0900

    create Index components for general and admin.

---
+++ resources/assets/js/components/admin/Index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>This is admin/Index component</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Index',
+    }
+</script>
+
+<style scoped>
+
+</style>
