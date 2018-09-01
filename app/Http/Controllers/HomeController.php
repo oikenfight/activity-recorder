35879f0747e19ae0c7eb3269a08ece18e500095d
YutaOikawa <oikenfight@gmail.com>
2018-09-01 18:09:27 +0900

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * Class HomeController
+ *
+ * @package App\Http\Controllers
+ */
+final class HomeController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
