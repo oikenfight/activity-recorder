0a71bf02ea40828173dc6b1edde6484ea37423e4
YutaOikawa <oikenfight@gmail.com>
2018-09-02 23:44:48 +0900

    add api to get all actions.

--- app/Http/Controllers/Api/ActionsController.php
+++ app/Http/Controllers/Api/ActionsController.php
@@ -1,11 +1,32 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class ActionsController extends Controller
+/**
+ * Class ActionsController
+ * @package App\Http\Controllers\Api
+ */
+final class ActionsController extends Controller
 {
-    //
+    /**
+     * @return array
+     */
+    public function all()
+    {
+        // TODO: get from database
+        $actions = [
+            ['id' => 0, 'name' => 'name0'],
+            ['id' => 1, 'name' => 'name1'],
+            ['id' => 2, 'name' => 'name2'],
+            ['id' => 3, 'name' => 'name3'],
+        ];
+
+        return [
+            'actions' => $actions,
+        ];
+    }
 }
