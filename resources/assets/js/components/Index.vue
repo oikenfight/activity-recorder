33e2bb70e607c3b6cc948b2f85ef89c9392e877d
YutaOikawa <oikenfight@gmail.com>
2018-09-02 17:40:05 +0900

    add app-nav.

--- resources/assets/js/components/Index.vue
+++ resources/assets/js/components/Index.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
+        <app-nav></app-nav>
         <h2>This is Index component</h2>
     </div>
 </template>
 
 <script>
+    import Nav from './Nav';
+
     export default {
-        name: 'Index',
+        data () {
+            return {}
+        },
+        components: {
+            'app-nav': Nav,
+        }
     }
 </script>
 
---
+++ resources/assets/js/components/Nav.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <!-- 切替ボタン -->
+        <nav class="navbar navbar-light navbar-laravel">
+            <button type="button" class="navbar-toggler navbar-nav ml-auto" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="折り畳みナビゲーション">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </nav>
+
+        <div class="collapse navbar navbar-light navbar-laravel" id="navbarToggleExternalContent">
+            <ul class="list-group col-12">
+                <li class="list-group-item text-center">
+                    <a class="text-secondary" href="/admin">管理画面へ</a>
+                </li>
+                <li class="list-group-item text-center">
+                    <a class="text-secondary" href="/admin">管理画面へ</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                authUser: 'auth user',
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
