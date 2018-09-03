40b9f362439e0a7cf5f3207451ebb6c4a9d81931
YutaOikawa <oikenfight@gmail.com>
2018-09-03 17:29:29 +0900

    create Thanks component.

---
+++ resources/assets/js/components/Thanks.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-10 card text-center">
+            <div class="card-body">
+                <h1 class="card-title">Thank you so much !!</h1>
+                <div class="col-12 text-center" style="padding-top: 50px">
+                    <button type="button" class="btn btn-light btn-lg" @click="checkMessage"><i class="fa fa-check-circle fa-3x" style="color:#38c172;"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {}
+        },
+        methods: {
+            checkMessage () {
+                this.$store.dispatch('statusReady')
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
