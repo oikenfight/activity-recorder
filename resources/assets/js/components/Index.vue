eceee6f9ebd811217a61634b8557e12878c6295a
YutaOikawa <oikenfight@gmail.com>
2018-09-03 17:09:57 +0900

    update Index component.

--- resources/assets/js/components/Index.vue
+++ resources/assets/js/components/Index.vue
@@ -3,7 +3,10 @@
         <app-nav></app-nav>
         <div class="container">
             <app-selector></app-selector>
-            <router-view></router-view>
+            <app-record v-if="showRecord"></app-record>
+            <app-confirmation v-if="showConfirmation"></app-confirmation>
+            <app-thanks v-if="showThanks"></app-thanks>
+            <!--<router-view></router-view>-->
         </div>
     </div>
 </template>
@@ -11,6 +14,10 @@
 <script>
     import Nav from './Nav';
     import Selector from './Selector'
+    import Record from './Record'
+    import Confirmation from './Confirmation'
+    import Thanks from './Thanks'
+    import { mapGetters } from 'vuex'
 
     export default {
         data () {
@@ -19,9 +26,27 @@
         components: {
             'app-nav': Nav,
             'app-selector': Selector,
+            'app-record': Record,
+            'app-confirmation': Confirmation,
+            'app-thanks': Thanks,
         },
         beforeCreate () {
             this.$store.dispatch('allActions')
+        },
+        computed: {
+            // store の getter をローカルにマッピングさせることで算出可能にしている。
+            ...mapGetters({
+                status: 'status',
+            }),
+            showRecord () {
+                return this.status === 'ready' || this.status === 'started'
+            },
+            showConfirmation () {
+                return this.status === 'stopped'
+            },
+            showThanks () {
+                return this.status === 'done'
+            },
         },
     }
 </script>
