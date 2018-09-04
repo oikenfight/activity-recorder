967ce8733049a7a89b388d41b32edeb07f994d69
YutaOikawa <oikenfight@gmail.com>
2018-09-04 19:26:18 +0900

    create Request for Admin/Action.

---
+++ app/Http/Requests/Action/Request.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Requests\Action;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class Request extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'action.name' => [
+                'required',
+                'min:2',
+                'max:128',
+                'alpha_dash'
+            ],
+            'action.japanese_name' => [
+                'required',
+                'min:2',
+                'max:128',
+            ],
+        ];
+    }
+}
