7307434e544a56311f93233fedc20c8d59322543
YutaOikawa <oikenfight@gmail.com>
2018-10-08 15:24:52 +0900

    modify Confirmation component using new vuex.

--- resources/assets/js/components/Confirmation.vue
+++ resources/assets/js/components/Confirmation.vue
@@ -3,11 +3,13 @@
         <div class="col-12 card text-center">
             <div class="card-body">
                 <!-- アスペクト比は stair actions に合わせてある -->
-                <video id="playback_video" class="embed-responsive embed-responsive-4by3" autoplay="1" style="border: 1px solid;"></video>
+                <video id="playback_video" class="embed-responsive embed-responsive-4by3" style="border: 1px solid;"></video>
             </div>
         </div>
 
         <div class="col-12 text-center">
+            <button v-if="!playing" type="button" class="btn btn-light btn-lg" @click="startPlay"><i class="fa fa-play-circle fa-3x" style="color:#38c172;"></i></button>
+            <button v-if="playing" type="button" class="btn btn-light btn-lg" @click="stopPlay"><i class="fa fa-pause-circle fa-3x" style="color:#38c172;"></i></button>
             <button type="button" class="btn btn-light btn-lg" @click="upload"><i class="fa fa-arrow-circle-o-up fa-3x" style="color:#38c172;"></i></button>
         </div>
     </div>
@@ -18,7 +20,14 @@
 
     export default {
         data () {
-            return {}
+            return {
+                video: null,
+                playing: false
+            }
+        },
+        mounted () {
+            this.video = document.getElementById('playback_video')
+            this.playInit()
         },
         computed: {
             // store の getter をローカルにマッピングさせることで算出可能にしている。
@@ -29,9 +38,22 @@
             }),
         },
         methods: {
+            playInit () {
+                this.video.pause()
+                this.video.currentTime = 0
+            },
+            startPlay () {
+                this.playing = true
+                this.video.play()
+            },
+            stopPlay () {
+                this.playing = false
+                this.playInit()
+            },
             upload () {
-                this.$store.dispatch('statusDone')
                 // TODO: add action to upload video.
+
+                this.$store.dispatch('statusUploaded')
             },
         },
     }
