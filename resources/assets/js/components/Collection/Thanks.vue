5d7b1dda68df19432fc29d4c2517b014f02a1ce7
YutaOikawa <oikenfight@gmail.com>
2018-10-09 19:59:10 +0900

    move it, and fix to setup recorder after uploaded.

--- resources/assets/js/components/Collection/Thanks.vue
+++ resources/assets/js/components/Collection/Thanks.vue
@@ -4,7 +4,7 @@
             <div class="card-body">
                 <h1 class="card-title">Thank you so much !!</h1>
                 <div class="col-12 text-center" style="padding-top: 50px">
-                    <button type="button" class="btn btn-light btn-lg" @click="checkMessage"><i class="fa fa-check-circle fa-3x" style="color:#38c172;"></i></button>
+                    <button type="button" class="btn btn-light btn-lg" @click="checkMessage"><i class="fa fa-check-circle fa-5x" style="color:#38c172;"></i></button>
                 </div>
             </div>
         </div>
@@ -12,13 +12,24 @@
 </template>
 
 <script>
+    import { mapGetters } from 'vuex'
+
     export default {
         data () {
             return {}
         },
+        computed: {
+            // store の getter をローカルにマッピングさせることで算出可能にしている。
+            ...mapGetters({
+                status: 'status',
+                recorder: 'recorder',
+            }),
+        },
         methods: {
             checkMessage () {
                 this.$store.dispatch('statusInit')
+                this.recorder.clear()
+                this.$router.push({ path: '/' });
             },
         }
     }
