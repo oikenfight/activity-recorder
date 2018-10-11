5a72614afe0dff72779c4612af5c77629cd04644
YutaOikawa <oikenfight@gmail.com>
2018-10-11 18:36:18 +0900

    add timer into Record.

--- resources/assets/js/components/Collection/Record.vue
+++ resources/assets/js/components/Collection/Record.vue
@@ -4,9 +4,12 @@
         <router-view></router-view>
 
         <!-- Main component -->
-        <div class="row">
+        <div class="row" style="padding-top: 20px;">
             <div class="col-12 card text-center">
                 <div class="card-body">
+                    <div v-if="showStop" class="float-right mr-3>
+                        <h3 class="text-danger">Timer: {{ recordingTime }}</h3>
+                    </div>
                     <!-- アスペクト比は stair actions に合わせてある -->
                     <video id="local_video" class="embed-responsive embed-responsive-4by3" autoplay="1" style="border: 1px solid;"></video>
                 </div>
@@ -27,6 +30,8 @@
         data () {
             return {
                 recording: false,
+                recordingTime: 0,
+                timer: null,
             }
         },
         mounted () {
@@ -50,12 +55,23 @@
                 console.log('here is startRecord method')
                 this.$store.dispatch('statusRecording')
                 this.recorder.start()
+                this.timer = this.startTimer()
             },
             stopRecord () {
                 console.log('here is stopRecord method')
                 this.$store.dispatch('statusRecorded')
                 this.recorder.stop()
                 this.$router.push({ path: 'confirmation' });
+            },
+            startTimer () {
+                setInterval(() => {
+                    this.recordingTime += 1
+                }, 1000)
+            },
+            stopTimer () {
+                if (this.timer) {
+                    clearInterval(this.timer);
+                }
             },
         }
 
